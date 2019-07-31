f598212062a11c7c461af692059be7301b0c033a
Telma Tavares <telmafilipa001@gmail.com>
2019-08-01 00:39:11 +0100

    vue-userdata -1

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <router-link class="navbar-brand" :to="'/'">User Data</router-link>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <router-link class="nav-link" :to="'/'">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" :to="'/contact'">Contact</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  import Home from './components/Home'
+  import PostsExample from './components/Posts'
+  import Contact from './components/Contact.vue'
+  export default {
+    name: 'App',
+    components: {
+      Home,
+      PostsExample,
+      Contact
+    }
+  }
+</script>
+
+<style>
+  @import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+</style>
---
+++ src/components/Posts.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="">
+    <table class="table table-striped mt-4">
+      <thead>
+      <tr>
+        <th scope="col">User Id</th>
+        <th scope="col">Title</th>
+        <th scope="col">Body</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="post in posts" :key="post.id">
+        <td >{{post.id}}</td>
+        <td >{{post.title}}</td>
+        <td>{{post.body}}</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<style scoped>
+    table {
+  border: 1px solid #ccc;
+  border-collapse: collapse;
+  table-layout: fixed;
+  width: 80%;
+  margin: 0 auto;
+}
+table tr {
+  border: 1px solid #ddd;
+  padding: .35em;
+  text-align: center;
+}
+table tr:nth-child(even) {
+  background: #f8f8f8;  
+}
+table th,
+table td {
+  padding: 10px;
+  text-align: center;
+}
+table th {
+  background: #00007f;
+  color: #fff;
+  font-size: .85em;
+  letter-spacing: .1em;
+  text-transform: uppercase;
+}
+table td {
+  white-space:normal;
+  overflow: hidden;
+  text-overflow:ellipsis;
+  text-align: center;
+}
+</style>
+
+<script>
+  import axios from 'axios'
+
+  export default{
+      name: 'PostsExample',
+      data(){
+
+          return {
+
+            posts: []
+
+          }
+      },
+
+    created(){
+        axios.get('https://jsonplaceholder.typicode.com/posts').then(posts=>{
+            this.posts = posts.data
+        });
+    }
+  }
+</script>
+
